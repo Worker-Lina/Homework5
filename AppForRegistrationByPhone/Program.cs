4fa9165aa56b8c61b5e627c33bb94d38e7ca1bed
алина человечкова <alinasmart19@mail.ru>
2020-12-19 12:59:53 +0600

    Добавьте файлы проекта.

---
+++ AppForRegistrationByPhone/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using Users;
+using System.Text.RegularExpressions;
+using ConsoleMenu;
+using System.Collections.Generic;
+
+namespace AppForRegistrationByPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accountSid = "AC03798a1882921c7f150d85a1f9967***";
+            string authToken = "ad39026ddd99dc8700deba5d111d5***";
+            TwilioClient.Init(accountSid, authToken);
+
+            List<User> users = new List<User>
+            {
+                new User("Мария","Цветаева","+77478902343",235467),
+                new User("Дмитрий","Коршунов","+77471233454",902376),
+                new User("Юрий","Шатунов","+77078901020",709345),
+            };
+
+
+            string[] items = { "Войти", "зарегистрироваться", "выйти" };
+            var menu = new Menu(items);
+            string choice;
+            do
+            {
+                Console.Clear();
+                menu.Print();
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.Write("Введите номер: ");
+                        string phone = Console.ReadLine();
+                        Console.Write("Введите пароль: ");
+                        int password = int.Parse(Console.ReadLine());
+
+                        bool succesful = false;
+                        foreach(User user in users)
+                        {
+                            if(user.GetPhone()==phone && user.GetPassword() == password)
+                            {
+                                succesful = true;
+                                Console.WriteLine($"{user.GetName()}, рады Вас видеть!");
+                            }
+                        }
+
+                        if (!succesful)
+                        {
+                            Console.WriteLine("Произошла ошибка, попробуйте вновь");
+                        }
+                        break;
+                    case "2":
+                        {
+                            Console.Clear();
+                            Console.Write("Имя: ");
+                            string name = Console.ReadLine();
+                            Console.Write("Фамилия: ");
+                            string surname = Console.ReadLine();
+
+                            string pattern = "[+]{1}[7]{1}[0-9]{3}[0-9]{3}[0-9]{4}";
+                            while (true)
+                            {
+                                Console.WriteLine("Введите номер в формате | +7XXXXXXXXXX  |");
+                                phone = Console.ReadLine();
+                                if (Regex.IsMatch(phone, pattern, RegexOptions.IgnoreCase))
+                                {
+                                    Console.WriteLine("ok");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректно введен номер");
+                                }
+                            }
+
+
+                            Random random = new Random();
+                            int randomCode = random.Next(100000, 999999);
+                            string code = randomCode.ToString();
+
+                            var to = new PhoneNumber(phone);
+                            var from = new PhoneNumber("+12059473430");
+
+                            var message = MessageResource.Create(
+                                to: to,
+                                from: from,
+                                body: code
+                            );
+
+                            Console.WriteLine("На ваш номер был выслан код");
+                            Console.Write("Введите код для подверждения: ");
+                            int inputCode = int.Parse(Console.ReadLine());
+
+                            if (randomCode == inputCode)
+                            {
+                                users.Add(new User(name, surname, phone,randomCode));
+                                Console.WriteLine($"{name}, Ваша регистрация прошла успешно!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Произошла ошибка, повторите попытку");
+                            }
+                        }
+                        break;
+                }
+                Console.ReadKey();
+            } while (choice != "3");
+        }
+    }
+}
---
+++ Users/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Users
+{
+    public class User
+    {
+        private string name;
+        private string surname;
+        private string phone;
+        private int password;
+
+        public User() { }
+        public User(string name, string surname, string phone,int password)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.phone = phone;
+            this.password = password;
+        }
+
+        public string GetPhone()
+        {
+            return phone;
+        }
+        public void SetPhone(string phone)
+        {
+            this.phone = phone;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetSurname(string surname)
+        {
+            this.surname = surname;
+        }
+        public int GetPassword()
+        {
+            return password;
+        }
+    }
+}
